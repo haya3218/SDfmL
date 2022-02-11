c6023cd642a8ada8a7f2ad4e562ab3ea756414f5
/nick haya <74699483+The-SGPT@users.noreply.github.com>
2022-02-11 10:10:15 +0800

    Make arg parser work actually

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,17 +8,20 @@
  * @copyright Copyright (c) 2022
  * 
  */
-#define SDL_MAIN_HANDLED // needs to be here so that SDL doesnt hijack our main function
 #define SDL_STBIMAGE_IMPLEMENTATION // to use sdl_stbimage
 #undef _HAS_STD_BYTE // avoid std::byte hijacking native byte type
 #include <iostream>
 #include "SDL2/SDL.h"
+#undef main
 #include "Render.hpp"
 #include <cmath>
 #include <Windows.h>
+#include <stdlib.h>
+#include "argh.h"
 
 using namespace std;
 using namespace Render;
+using namespace argh;
 
 class MainState : public State {
     AnimatedObject objs;
@@ -141,14 +144,101 @@ class SplashScreen : public State {
 
 SplashScreen m;
 
-int main() {
+int main(int argc, char* argv[]) 
+{
     if (!Init("SDfmL :))))))")){
-        return ERROR_CODE;
+        return EXIT_FAILURE;
     }
+
+    parser pa(argc, argv);
+    if (pa[{"-v", "--verbose"}])
+        log("", "Verbose mode enabled.", NORMAL, __FILENAME__, __LINE__);
 
     SwitchState(&m);
 
     if (!Update()) {
-        return NO_ERROR_CODE;
+        return EXIT_SUCCESS;
     }
+    return EXIT_SUCCESS;
+}
+
+// This is the main entry point of a program. Currently all this does is parse arguments and call main().
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+{
+    int    argc;
+    char** argv;
+
+    char*  arg;
+    int    index;
+    int    result;
+
+    // count the arguments
+    
+    argc = 1;
+    arg  = cmdline;
+    
+    while (arg[0] != 0) {
+
+        while (arg[0] != 0 && arg[0] == ' ') {
+            arg++;
+        }
+
+        if (arg[0] != 0) {
+        
+            argc++;
+        
+            while (arg[0] != 0 && arg[0] != ' ') {
+                arg++;
+            }
+        
+        }
+    
+    }    
+    
+    // tokenize the arguments
+
+    argv = (char**)malloc(argc * sizeof(char*));
+
+    arg = cmdline;
+    index = 1;
+
+    while (arg[0] != 0) {
+
+        while (arg[0] != 0 && arg[0] == ' ') {
+            arg++;
+        }
+
+        if (arg[0] != 0) {
+        
+            argv[index] = arg;
+            index++;
+        
+            while (arg[0] != 0 && arg[0] != ' ') {
+                arg++;
+            }
+        
+            if (arg[0] != 0) {
+                arg[0] = 0;    
+                arg++;
+            }
+        
+        }
+    
+    }    
+
+    // put the program name into argv[0]
+
+    char filename[_MAX_PATH];
+    
+    GetModuleFileName(NULL, filename, _MAX_PATH);
+    argv[0] = filename;
+
+    // call the user specified main function   
+
+    log("", to_string(argc));
+    
+    result = main(argc, argv);
+    
+    free(argv);
+    return result;
 }
